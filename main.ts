37caf594db0cb686ac267645c6037b108119d077
Gerard Braad <me@gbraad.nl>
2025-04-02 22:26:00 +0800

    Center in the middle

--- main.ts
+++ main.ts
@@ -60,7 +60,7 @@ class AnalogueClockView extends ItemView {
 					align-items: center;
 					justify-content: center;
 					width: 100%;
-					height: 100%;
+					height: 100vh;
 				}
 
 				.clock {
